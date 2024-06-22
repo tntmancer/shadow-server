d28ee96f40ecc91fdaa68f07e0b9e807c883ccf2
tntmancer <tntmancer@gmail.com>
2024-06-22 19:35:48 -0400

    got post and reply creation working!

--- Profiles/routes.js
+++ Profiles/routes.js
@@ -78,9 +78,9 @@ export default function ProfileRoutes(app) {
     // Account Authentication Management
     const signin = async (req, res) => {
         const { username, password } = req.body;
+        console.log(username, password);
         const currentProfile
             = await dao.findProfileByCredentials(username, password);
-
         if (currentProfile) {
             req.session["currentProfile"] = currentProfile;
             res.json(currentProfile);
